3a8b4cc48b91e4fa6a9cbbda4d86b31d72b8b995
lzxb <1340641314@qq.com>
2020-05-06 11:10:00 +0800

    fix: 修复title错误

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     base: '/genesis-docs/',
+    title: 'Genesis',
     themeConfig: {
-      title: 'Genesis',
       repo: 'fmfe/genesis',
       repoLabel: 'Github',
       nav: [
