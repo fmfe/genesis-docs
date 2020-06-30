e659375fea45f1ba17e49cd95afb8a70b1dc2069
Anish <anishgeorge2690@gmail.com>
2020-06-30 21:15:39 +0530

    added option to switch between doc

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,61 +1,68 @@
 module.exports = {
-    base: '/genesis-docs/',
-    title: 'Vue Genesis',
-    description: '基于 Vue SSR 的微前端、微服务、轻量级的解决方案',
-    themeConfig: {
-      repo: 'fmfe/genesis',
-      repoLabel: 'Github',
-      nav: [
-        { text: '首页', link: '/' },
-        { text: '指南', link: '/guide/' },
-        {
-          text: '核心库',
-          items: [
-            { text: 'genesis-core', link: '/core/' },
-            { text: 'genesis-compiler', link: '/compiler/' },
-            { text: 'genesis-app', link: '/app/' },
-            { text: 'genesis-remote', link: '/remote/' },
-          ]
-        },
-        { text: '官方博客', link: '/blog/followme5.0' },
-      ],
-      sidebar: [
-        '/',
-        {
-          title: '指南',
-          path: '/guide/',
-          collapsable: false,
-          children: [
-            '/guide/introduce',
-            '/guide/',
-          ]
-        },
-        {
-          title: '进阶',
-          path: '/guide/',
-          collapsable: false,
-          children: [
-            '/guide/renderer',
-            '/guide/micro',
-            '/guide/meta',
-            '/guide/webpack',
-            '/guide/babel',
-            '/guide/postcss',
-          ]
-        },
-        {
-          title: '官方博客',
-          path: '/blog/',
-          collapsable: false,
-          children: [
-            '/blog/followme5.0',
-            '/blog/2020-05-25',
-            '/blog/2020-06-18',
-            '/blog/2020-06-27',
-          ]
-        }
-      ]
-    }
+  base: '/genesis-docs/',
+  title: 'Vue Genesis',
+  description: '基于 Vue SSR 的微前端、微服务、轻量级的解决方案',
+  themeConfig: {
+    repo: 'fmfe/genesis',
+    repoLabel: 'Github',
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '指南', link: '/guide/' },
+      {
+        text: '核心库',
+        items: [
+          { text: 'genesis-core', link: '/core/' },
+          { text: 'genesis-compiler', link: '/compiler/' },
+          { text: 'genesis-app', link: '/app/' },
+          { text: 'genesis-remote', link: '/remote/' },
+        ]
+      },
+      { text: '官方博客', link: '/blog/followme5.0' },
+      {
+        text: "选择语言",
+        items: [
+          { text: "English", link: "https://anish2690.github.io/genesis-docs-en/", target: '_self', rel: '' },
+          { text: "简体中文", link: "https://fmfe.github.io/genesis-docs/guide/", target: '_self', rel: '' },
+        ],
+      },
+
+    ],
+    sidebar: [
+      '/',
+      {
+        title: '指南',
+        path: '/guide/',
+        collapsable: false,
+        children: [
+          '/guide/introduce',
+          '/guide/',
+        ]
+      },
+      {
+        title: '进阶',
+        path: '/guide/',
+        collapsable: false,
+        children: [
+          '/guide/renderer',
+          '/guide/micro',
+          '/guide/meta',
+          '/guide/webpack',
+          '/guide/babel',
+          '/guide/postcss',
+        ]
+      },
+      {
+        title: '官方博客',
+        path: '/blog/',
+        collapsable: false,
+        children: [
+          '/blog/followme5.0',
+          '/blog/2020-05-25',
+          '/blog/2020-06-18',
+          '/blog/2020-06-27',
+        ]
+      }
+    ]
   }
-  
-  
+}
+
